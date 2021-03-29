13310697f34ac0f501dd4ff921068e98c4317b70
eleacarton <eleacarton@me.com>
2021-03-29 12:05:18 +0200

    connect db

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongo: {
-        uri:"mongodb+srv://grp1:P@ssword1234@cluster0.1hgfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        uri: process.env.DB_URI
     }
 }
