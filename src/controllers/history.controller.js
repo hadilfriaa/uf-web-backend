34f65f62795553bd35ce9bd4dc14090af58c06bc
eleacarton <eleacarton@me.com>
2021-05-03 14:25:10 +0200

    history

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -10,53 +10,51 @@ exports.create = (req, res) => {
         product: req.body.product
     });
 
-    history.save()
-    .then((data) => {
-        res.send({
-            data: data,
-        })
-    })
-    .catch((err) => {
-        console.log(err.message);    
-        res.status(500).send({
+    Product.findOne({
+      _id: req.body.product
+    }).then(
+      (product) => {
+        const lastPrice = product.price;
+        if(req.body.priceH <= lastPrice){
+          res.status(500).send({
             error: 500,
-            message: err.message || "some error occured while creating order"
-        })
-    })
-
-};
-
-
-exports.getAll = (req, res) => {
-    History.find()
-    .then(
-      (Historys) => {
-        res.status(200).json(Historys);
+            message: "Vous ne pouvez pas faire une offre d'un montant inférieur à l'offre précédente"
+          })
+        }else{
+          history.save()
+          .then((data) => {
+            Product.findByIdAndUpdate(req.body.product, {$push: {history: data._id}}).then(() => {
+              res.send({
+                  data: data,
+              })
+              .catch((err) => res.send(err));
+            });
+            Product.findByIdAndUpdate(req.body.product, {price: data.priceH}).then(() => {
+              res.send({
+                  data: data,
+              })
+              .catch((err) => res.send(err));
+            });
+            res.send({
+              data: data,
+            })
+          })
+          .catch((err) => {
+              console.log(err.message);    
+              res.status(500).send({
+                  error: 500,
+                  message: err.message || "some error occured while creating order"
+              })
+          })
+        }
       }
     ).catch(
       (error) => {
-        res.status(400).json({
+        res.status(404).json({
           error: error
         });
       }
     );
-};
-
-
-exports.getOne = (req, res) => {
-  History.findOne({
-    _id: req.params.id
-  }).then(
-    (history) => {
-      res.status(200).json(history);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
 };
 
 exports.delete = (req, res) => {
@@ -74,21 +72,6 @@ exports.delete = (req, res) => {
       .catch((err) => res.send(err));
     
   };
-
-  // exports.getAllByIdUser = (req, res) => {
-  //   History.find({"user": req.params.id})
-  //   .then(
-  //     (Historys) => {
-  //       res.status(200).json(Historys);
-  //     }
-  //   ).catch(
-  //     (error) => {
-  //       res.status(400).json({
-  //         error: error
-  //       });
-  //     }
-  //   );
-  // };
 
   exports.getAllByIdProduct = (req, res) => {
     History.find({"product": req.params.id})
--- src/routes/history.route.js
+++ src/routes/history.route.js
@@ -4,10 +4,7 @@ const history = require('../controllers/history.controller');
 const verifyAdmin = require ('../middlewares/verifyAdmin');
 const verifyToken = require('../middlewares/verifyToken');
 
-
-router.get('/history/:id', history.getOne);
-router.post('/history', verifyAdmin, history.create);
-router.get('/history', history.getAll);
+router.post('/history', verifyToken, history.create);
 router.delete('/history/:id', verifyAdmin, history.delete);
 router.get('/history/product/:id', verifyToken, history.getAllByIdProduct);
 
